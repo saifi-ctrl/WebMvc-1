a9942b04d489c3b430c51296415ca9928d548627
saifi-ctrl <firstnamesaifali@gmail.com>
2024-08-06 20:19:58 +0530

    Bug 12 Fix

--- demo.java
+++ demo.java
@@ -1,5 +1,10 @@
 class Demo{
 public static void main(String[]args){
 System.out.println("Say Yes");
+System.out.print("Accept");
 }
+}
+class TestDemo{
+public static void main(String[]args){
+System.out.println("Just for Testing");
 }
